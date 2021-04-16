884751281353f8abb3a19cb45653f06edfa8155d
Sliganyth <Sliganyth@gmail.com>
2021-04-16 19:46:11 +0200

    Update

--- Autisto.py
+++ Autisto.py
@@ -1,33 +1,141 @@
 import discord
+from discord.ext import commands
+import youtube_dl
+import os
 
+client = commands.Bot(command_prefix="*")
 
-client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('The Autist Is Here')
+    print('''
+...................../´¯¯/)
+...................,/¯.../
+.................../..../
+.............../´¯/'..'/´¯¯`·¸
+.........../'/.../..../....../¨¯)
+..........('(....´...´... ¯~/'..')
+...........\..............'...../
+............\....\.........._.·´
+.............\..............(
+..............\..............)
+''')
 
-@client.event
-async def on_message(message):
-    message.content = message.content.lower()
-    if message.author == client.user:
+
+@client.command()
+async def stifler(ctx):
+    print(ctx.author)
+    print(ctx.message)
+    print(ctx.guild)
+
+
+@client.command()
+async def fuckyou(ctx):
+    print(ctx.author)
+    print(ctx.message)
+    print(ctx.guild)
+
+
+@client.command()
+async def pedo(ctx):
+    print(ctx.author)
+    print(ctx.message)
+    print(ctx.guild)
+
+
+@client.command()
+async def weeb(ctx):
+    print(ctx.author)
+    print(ctx.message)
+    print(ctx.guild)
+
+
+@client.command()
+async def server(ctx):
+    name = str(ctx.guild.name)
+    description = str(ctx.guild.description)
+
+    owner = str(ctx.guild.owner)
+    id = str(ctx.guild.id)
+    region = str(ctx.guild.region)
+    memberCount = str(ctx.guild.member_count)
+
+    icon = str(ctx.guild.icon_url)
+
+    embed = discord.Embed(
+        title=name + " Server Information",
+        description=description,
+        color=discord.Color.blue()
+    )
+    embed.set_thumbnail(url=icon)
+    embed.add_field(name="Owner", value=owner, inline=True)
+    embed.add_field(name="Server ID", value=id, inline=True)
+    embed.add_field(name="Region", value=region, inline=True)
+    embed.add_field(name="Member Count", value=memberCount, inline=True)
+
+    await ctx.send(embed=embed)
+
+
+@client.command()
+async def play(ctx, url: str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
         return
-    if message.content.startswith("hello"):
 
-        if str(message.author) == "Sliganyth#0698":
-            await message.channel.send("Hello you are a dipshit")
-        else:
-            await message.channel.send("Hello, Dipshit")
-    if message.content.startswith("Fuck You"):
-        await message.channel.send("Fuck You Too")
-    if message.content.startswith("ip"):
-        await message.channel.send("82.217.224.164:25701")
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+    await voiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
-@client.event
-async def on_message(message):
-    if str(message.channel) == "memes" and message.content != "":
-        await message.channel.purge(limit=1)
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
 
 
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
 
-client.run('')
+
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused.")
+
+
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    voice.stop()
+
+client.run("NjkyNzIwODg0NzI4NzI1NTE1.XnyomQ.Qyl5klFMkfjqbq9vR8ZgyW12nDk")
